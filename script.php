fff05cae6ea1bbf67c7a764c660b4f6490d99eb8
Ahmed Abdelaziz <ahmed.abdelaziz@code95.com>
2019-11-14 13:47:13 +0200

    Test

--- script.php
+++ script.php
@@ -1,13 +1,11 @@
 #!/usr/bin/php
 <?php
+echo "installing Project ...............";
+echo "\n \n \n";
 
-echo "installing bedrock ......";
+
 shell_exec("composer create-project roots/bedrock ");
-echo "bedrock installed";
-echo "change Dir ...";
 shell_exec("cd bedrock/web/app/themes");
-echo "Go To theme folder done";
-echo "installing Sage ....";
 shell_exec("composer create-project roots/sage");
-echo "sage installed";
-shell_exec("clean");
+echo "\n \n \n";
+echo "Project Install Done";
