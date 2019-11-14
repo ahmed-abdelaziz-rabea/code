3c25fab718f276b249138bb801f91f880824e951
Ahmed Abdelaziz <ahmed.abdelaziz@code95.com>
2019-11-14 14:32:40 +0200

    Test

--- script.php
+++ script.php
@@ -2,9 +2,8 @@
 <?php
 echo "installing Project ...............";
 echo "\n \n \n";
-
-
 shell_exec("composer create-project roots/bedrock && cd bedrock/web/app/themes && composer create-project roots/sage");
-
-echo "\n \n \n";
-echo "Project Install Done";
+shell_exec("cd bedrock/web/app/themes/sage && git clone git@git.code95.info:code95/starter-theme.git framework");
+shell_exec("cp -v functions.php bedrock/web/app/themes/sage/resources");
+echo "Function copy done ...............";
+echo "\n";
