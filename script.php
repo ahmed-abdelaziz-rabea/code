9af3544628e5a85e8b76df83a165a7463b19ed5d
Ahmed Abdelaziz <ahmed.abdelaziz@code95.com>
2019-11-14 12:38:03 +0200

    Test

---
+++ script.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+
+echo "installing bedrock ......";
+shell_exec("composer create-project roots/bedrock ");
+echo "bedrock installed";
+echo "change Dir ...";
+shell_exec("cd bedrock/web/app/themes");
+echo "Go To theme folder done";
+echo "installing Sage ....";
+shell_exec("&& composer create-project roots/sage");
+echo "sage installed";
